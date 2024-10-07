7899ff7b38a80a4265dd450890034810218f59fc
=elangyudha280 <elangyudharakasiwi2@gmail.com>
2024-10-07 14:06:19 -0700

    Selesai setup project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,24 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        c:{
+          green:{
+            '200':'#4CAF4F',
+            '700':'#103E13'
+          },
+          slate:{
+            '200':'#E0E0E0',
+            '400':'#455A64'
+          }
+        },
+      },
+      fontFamily: {
+        'poppins': ['"Poppins"','sans-serif'],
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 };
 export default config;
